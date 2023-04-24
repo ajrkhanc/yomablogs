542d8d45c457ffcee6030f93fc77b14d8178b3e8
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-04-24 16:56:35 +0530

    'chan

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-   database : `mongodb://ajrkhan:C17G2t6H9r3Flk7e@cluster0-shard-00-00.udm2r.mongodb.net:27017,cluster0-shard-00-01.udm2r.mongodb.net:27017,cluster0-shard-00-02.udm2r.mongodb.net:27017/yomadb?ssl=true&replicaSet=atlas-50qbk2-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    database: `mongodb+srv://ajrkhan:C17G2t6H9r3Flk7e@cluster0.udm2r.mongodb.net/yomadb?retryWrites=true&w=majority`,
+//    database : `mongodb://ajrkhan:C17G2t6H9r3Flk7e@cluster0-shard-00-00.udm2r.mongodb.net:27017,cluster0-shard-00-01.udm2r.mongodb.net:27017,cluster0-shard-00-02.udm2r.mongodb.net:27017/yomadb?ssl=true&replicaSet=atlas-50qbk2-shard-0&authSource=admin&retryWrites=true&w=majority`,
     superSecret: 'FuckJava'
 };
